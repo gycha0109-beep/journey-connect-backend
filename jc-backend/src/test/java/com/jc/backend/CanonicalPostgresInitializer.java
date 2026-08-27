0ddf63fc55560d25fd233808da00e85ec61b36d8
gycha0109-beep <gycha0109@gmail.com>
2026-08-27 12:55:11 +0900

    test(db): bootstrap PF6 canonical SQL

--- jc-backend/src/test/java/com/jc/backend/CanonicalPostgresInitializer.java
+++ jc-backend/src/test/java/com/jc/backend/CanonicalPostgresInitializer.java
@@ -50,7 +50,9 @@ public final class CanonicalPostgresInitializer
             "59_crew_tags.sql",
             "60_crew_tags_smoke_test.sql",
             "61_crew_recommendation_exposure.sql",
-            "62_crew_recommendation_exposure_smoke_test.sql");
+            "62_crew_recommendation_exposure_smoke_test.sql",
+            "63_crew_open_chat.sql",
+            "64_crew_open_chat_smoke_test.sql");
 
     private static final String EXTERNAL_URL = setting("jc.test.db.url", "JC_TEST_DB_URL", "");
     private static final String EXTERNAL_USERNAME = setting(
